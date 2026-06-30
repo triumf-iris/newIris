b5d3121a2e55a1bd53ce8f5722773a592090cb11
iris <iris@iris00.triumf.ca>
2026-06-30 16:50:43 -0700

    added runTree.sh for treeIris example

---
+++ testMacros/testSimulation/runTree.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/iris/curtis/NewIris/newIRIS/bin/treeIris -c=/home/iris/curtis/NewIris/newIRIS/Configurationfiles/phys_config_12C_2H_CATIMA.txt -o=tree010.root -raw /iris/data1/iris/July2022_S1701/iris_000095170000.mid.gz
+
